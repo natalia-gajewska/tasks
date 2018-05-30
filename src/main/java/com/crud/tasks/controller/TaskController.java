4554fd111ba89a3ce31e3ac02e1f4ae15b46ca8b
natalia.gajewska <natalia.gajewska@poczta.fm>
2018-05-30 10:49:17 +0200

    14.4 poprawione

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -17,20 +17,20 @@ public class TaskController {
     public List<TaskDto> getTask() {
         return new ArrayList<>();
     }
-@RequestMapping(method=RequestMethod.GET, value = "getTasks")
+@RequestMapping(method=RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
-@RequestMapping(method=RequestMethod.DELETE, value = "deleteTasks")
+@RequestMapping(method=RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId) {
 
     }
-@RequestMapping(method=RequestMethod.PUT, value = "uptadeTasks" )
+@RequestMapping(method=RequestMethod.PUT, value = "uptadeTask" )
     public TaskDto uptadeTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "Test content");
 
     }
-@RequestMapping(method=RequestMethod.POST, value = "createTasks")
+@RequestMapping(method=RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto taskDto) {
     }
 }
